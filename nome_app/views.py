9c8a4587c62b4f7c93b515eb8d7e4df1c9031626
William Souza <66651052+william-mjsouza@users.noreply.github.com>
2024-09-13 11:52:57 -0300

    Renderizando HTML via Django
    Criação do tamplate pra view da app.

--- nome_app/views.py
+++ nome_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render     # Biblioteca que permite renderizar um tamplate html
+from django.http import HttpResponse    # Biblioteca que tem a função HttResponse (retorna uma mensagem tag html)
 
-# Create your views here.
+# Todas as views são criadas aqui.
+# Cada view é uma função que recebe uma requisição e envia uma resposta
+# Para dizer quando esta função deve ser chamada, deve-se definir a rota dela
+def home(request):
+    return render(request, "nome_app/home.html")    # No caminho não deve-se colocar "templates" porque o Django já procura nesta pasta
