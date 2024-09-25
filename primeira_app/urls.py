1ba4be373a33432a825eaf7a41c961b70895aaeb
William Souza <66651052+william-mjsouza@users.noreply.github.com>
2024-09-24 23:34:37 -0300

    Melhora na camada de templates
    A organização foi melhorada e agora é possível ter navbars personalizáveis para cada app.

--- primeira_app/urls.py
+++ primeira_app/urls.py
@@ -4,6 +4,7 @@ from . import views  # importa todas as views da app
 
 # Aqui devem ficar todas as rotas da app
 urlpatterns = [
-    path('app_static_page', views.render_static_template, name='app_static_page'),  # Apelidei essa URL de app_static_page
-    path('app_dinamic_page', views.render_dinamic_template, name='app_dinamic_page')
+    path('', views.render_index_template, name='primeira_app'), # Apelidei essa URL de primeira_app (primeira_app = http://127.0.0.1:8000/primeira_app/)
+    path('pagina_estatica', views.render_static_template, name='pagina_estatica'), # pagina_estatica = http://127.0.0.1:8000/primeira_app/pagina_estatica
+    path('pagina_dinamica', views.render_dinamic_template, name='pagina_dinamica') # pagina_dinamica = http://127.0.0.1:8000/primeira_app/pagina_dinamica
 ]
--- primeira_app/views.py
+++ primeira_app/views.py
@@ -3,9 +3,14 @@ from django.shortcuts import render     # Biblioteca que permite renderizar um t
 # - Todas as views são criadas aqui;
 # - Cada view é uma função que recebe uma requisição HTTP e retorna uma resposta HTTP;
 # - Para dizer quando esta função deve ser chamada, deve-se definir a rota dela em urls.py.
+
+def render_index_template(request):
+    # Renderiza o arquivo index_primeira_app.html dentro de primeira_app/templates/primeira_app/
+    return render(request, "primeira_app/index_primeira_app.html")    # No caminho não deve-se colocar "templates" porque o Django já procura nesta pasta
+
 def render_static_template(request):
     # Renderiza o arquivo static_template_app.html dentro de primeira_app/templates/primeira_app/
-    return render(request, "primeira_app/static_template_app.html")    # No caminho não deve-se colocar "templates" porque o Django já procura nesta pasta
+    return render(request, "primeira_app/static_template_app.html")
 
 def render_dinamic_template(request):
 
